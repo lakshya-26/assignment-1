fc04e3a863968ad34016f403d90353a6462be85d
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-02-28 00:02:27 +0530

    text change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-red-600 w-[100%] lg:h-[100px] flex lg:flex-row flex-col items-center lg:justify-between px-20 text-white md:text-lg lg:py-0 py-8">
+    <div className="bg-red-600 w-[100%] lg:h-[100px] flex lg:flex-row flex-col items-center lg:justify-between px-20 text-white md:text-lg text-xs lg:py-0 py-8">
       <div>
       <i class="fa-solid fa-square-phone"></i>&nbsp;
       <span>Toll free <b>1800 200 1234</b></span>
